59ef5c686a42e7cfe41f1f8f6b347e8a9276f9e3
mjyhhh <634445437@qq.com>
2019-09-17 17:53:30 +0800

    完成user组件静态页

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
+import user from '../components/user.vue'
 
 Vue.use(Router)
 
@@ -21,7 +22,12 @@ export default new Router({
     {
       path: '/home',
       name: 'home',
-      component: Home
+      component: Home,
+      children:[{
+        path: '/user',
+        name: 'user',
+        component:user
+      }]
     }
     // {
     //   path: '/about',
